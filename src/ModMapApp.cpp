18ea929d971a5d59e9212c68882ad5ebac6e06b5
Giosué Russo <josh@jBook.local>
2014-08-11 18:21:20 +0200

    loading def model from start

--- src/ModMapApp.cpp
+++ src/ModMapApp.cpp
@@ -4,11 +4,23 @@
 #include "cinder/Font.h"
 #include "cinder/Camera.h"
 
+#include "cinder/gl/Texture.h"
+#include "cinder/gl/Fbo.h"
+#include "cinder/gl/Vbo.h"
+#include "cinder/gl/GlslProg.h"
+
+#include "cinder/ObjLoader.h"
+
+#include "cinder/Sphere.h"
+
 #include "cinderSyphon.h"
 #include "OscListener.h"
 
+#include "MeshHelper.h"
+
 #define WIDTH 1920
 #define HEIGHT 1080
+#define RES_DEF_OBJ		CINDER_RESOURCE( ./, cube.obj, 128, DATA )
 
 using namespace ci;
 using namespace ci::app;
@@ -34,6 +46,11 @@ class ModMapApp : public AppNative {
   Vec3f mEye;
   Vec3f mCenter;
   Vec3f mUp;
+  
+  TriMesh			  mMesh;
+	gl::VboMesh		mVBO;
+	gl::GlslProg	mShader;
+  
 };
 
 
@@ -46,12 +63,13 @@ void ModMapApp::prepareSettings(cinder::app::AppBasic::Settings *settings){
 void ModMapApp::setup()
 {
   //Camera
-  mCam.setPerspective( 60.0f, getWindowAspectRatio(), 5.0f, 3000.0f );
-  mEye = Vec3f( 0.0f, 0.0f, -500.0f );
+  mCam.setPerspective( 31.417, getWindowAspectRatio(), 5.0f, 3000.0f );
+  mEye = Vec3f( 0.0f, 0.0f, 3396.584);
   mCenter = Vec3f( 0.0f, 0.0f, 0.0f );
-  mUp = Vec3f::yAxis() *-1;
+  mUp = Vec3f::yAxis();
   mCam.lookAt( mEye, mCenter, mUp );
-  
+  mCam.setNearClip(10000);
+//  mCam.setFarClip(0);
   mFont = Font( "Arial", 120.0f );
   
   mSyphonOutA.setName("ModMap :: Camera A");
@@ -59,10 +77,28 @@ void ModMapApp::setup()
   
   mOSCParam_speed = 20;
   mRotation = 0;
+  
+  //Setup mVBO
+//  mMesh = MeshHelper::createSphere();
+//  mVBO = gl::VboMesh( mMesh );
+  
+  ObjLoader loader( loadResource( "./assets/cinderModMap001.obj" ) );
+//  ObjLoader loader( loadFile( "exa010.obj" ) );
+  loader.load( &mMesh, true );
+  mVBO = gl::VboMesh( mMesh );
 }
 
 void ModMapApp::keyDown( KeyEvent event ){
-  
+  if( event.getChar() == 'o' ) {
+		fs::path path = getOpenFilePath();
+		if( ! path.empty() ) {
+			ObjLoader loader( loadFile( path ) );
+			loader.load( &mMesh, true );
+			mVBO = gl::VboMesh( mMesh );
+			console() << "path" << path;
+      console() << "Total verts: " << mMesh.getVertices().size() << std::endl;
+		}
+	}
 }
 
 void ModMapApp::mouseDown( MouseEvent event )
@@ -91,16 +127,20 @@ void ModMapApp::update()
 
 void ModMapApp::draw()
 {
+  gl::enableDepthWrite();
+	gl::enableDepthRead();
 	// clear out the window with black
 	gl::clear( Color( 0, 0, 0 ) );
   gl::setMatrices( mCam );
   
   gl::color(0, 0.3, 0.9);
-  float r = getWindowWidth() * 0.1;
+//  float r = getWindowWidth() * 0.1;
 //  gl::drawSolidEllipse(getWindowCenter(), 100, 100);
   gl::pushMatrices();
-  gl::rotate(Vec3f(mRotation,mRotation * 0.73,0));
-  gl::drawSphere(Vec3f(0,0,0), r, 6);
+//  gl::rotate(Vec3f(0,mRotation,0));
+//  gl::scale(Vec3f(100,100,100));
+//  gl::drawSphere(Vec3f(0,0,0), r, 6);
+  gl::draw(mVBO);
   gl::popMatrices();
   
   mSyphonOutA.publishScreen();
